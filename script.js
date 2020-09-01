41aca2ffae21994c0313caf43850c8b738e925ad
Stoyan <68004209+rm1902ss@users.noreply.github.com>
2020-09-01 06:15:08 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ toggleButton.addEventListener('click', (e) => {
     e.preventDefault()
     navbarLinks.classList.toggle('active')
 })
+
+const showMoreLessBtn = document.querySelector('.show-more-less-btn')
+const containerLong = document.querySelector('#long')
+
+showMoreLessBtn.addEventListener('click', (event) => {
+    containerLong.classList.toggle('hide')
+
+    if(containerLong.classList.contains('hide')) {
+        showMoreLessBtn.innerHTML = 'Show more'
+    } else {
+        showMoreLessBtn.innerHTML = 'Show less'
+    }
+})
